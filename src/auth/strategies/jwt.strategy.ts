a12fe5fe4d3f9112a49dc1a1f2cc7bc7fa1b41f8
henwil <tcheutchouawilfried70@gmail.com>
2026-02-05 00:52:24 +0100

    last commit

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
  async validate(payload: any) {
   return {
-    userId: payload.sub,
+    id: payload.sub,
     mail: payload.mail,
     role: payload.role,
   };
